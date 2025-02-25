90eb8b6902acedfc59d48dc7da6282bc660e80f4
Jorge Mendes <jorge.mendes7@hotmail.com>
2025-02-24 22:49:38 -0300

    alteracao no useState e criacao do metodo para enviar o formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,53 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [comentarios, setComentarios] = useState(['Seja o primeiro a comentar'])
+const App = () => {
+  const [email, setEmail] = useState('')
+  const [comentario, setComentario] = useState('')
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault()
+
+    const novoComentario = {
+      id: Math.floor(Math.random() * 1000000),
+      email: email,
+      comentario: comentario,
+      createAt: new Date()
+    }
+
+    console.log(novoComentario)
+    setEmail('')
+    setComentario('')
+  }
+
 
   return (
     <div id='appContainer'>
       <h1>Seção de comentários</h1>
-      <form>
-        <label htmlFor="email">Email</label>
-        <input type="text" name='email'/>
-        <label htmlFor="comentario">Comentário</label>
-        <textarea name='comentario' rows='4'></textarea>
-        <input type="submit" name="enviar" value='Enviar comentário'/>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email" id='email'>Email</label>
+        <input 
+          type="text" 
+          name='email'
+          required
+          value={email}
+          onChange={(ev) => setEmail(ev.target.value)}
+        />
+        <label htmlFor="comentario" id='comentario'>Comentário</label>
+        <textarea 
+          name='comentario' 
+          rows='4'
+          required
+          value={comentario}
+          onChange={(ev) => setComentario(ev.target.value)}
+          ></textarea>
+        <button type='submit' id='submit'>Enviar comentário</button>
         <hr />
-        {comentarios.map((comentario, index) => (
-          <p key={index}>{comentario}</p>
-        ))}
+        <div id='areaDeComentarios' >
+          <h3>exemplo@email.com</h3>
+          <span>Em 01/01/2025</span>
+          <p>comentário de exemplo</p>
+        </div>
       </form>
     </div>
   )
